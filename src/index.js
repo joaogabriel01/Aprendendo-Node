ce9d55a30944ee996541f523d3e83676c4ce1912
joaoazevedo <joaoazevedo@brasilcard.net>
2022-01-13 17:00:32 -0300

    Aula 7: 20:31

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const {uuid} = require('uuidv4');
 
 const app = express();
 
@@ -7,41 +8,53 @@ app.use(express.json())
 // app.get('/projects', (request, response) => {
 //     return response.send('{message: Hello World!}');
 // });
+
+const projects = [];
+
 app.get('/projects', (request, response) => {
-    const query = request.query;
-    console.log(query);
-    return response.json([
-        'Projeto 1',
-        'Projeto 2',
-        'Projeto 3'
-    ]);
+
+    return response.json(projects);
 });
 
 app.post('/projects', (request, response) => {
-    const body = request.body;
-    console.log(body);
-    return response.json([
-        'Projeto 1',
-        'Projeto 2',
-        'Projeto 3',
-        'Projeto 4'
-    ]);
+    const { title , owner } = request.body;
+
+    const id = uuid();
+
+    const project = {
+        id,
+        title,
+        owner
+    };
+
+    projects.push(project)
+    return response.json(project);
 })
 
-app.put('/projects/:id/owner/:owner', (request, response) => {
+app.put('/projects/:id', (request, response) => {
 
-    const id = request.params; 
-    console.log(id);
+    const { id } = request.params;
+    const { title, owner } = request.body;  
 
-    return response.json([
-        'Projeto 5',
-        'Projeto 2',
-        'Projeto 3',
-        'Projeto 4'
-    ]);
+    const projectIndex = projects.findIndex(project=>project.id===id)
+
+    if(projectIndex < 0){
+        return response.status(400).json({ error: 'Project not found.' });
+    }
+
+    const project = {
+        id,
+        title,
+        owner
+    };
+
+    projects[projectIndex] = project;
+    console.log(id,title,owner);
+
+    return response.json(project);
 })
 
-app.delete('/projects', (request, response) => {
+app.delete('/projects/:id', (request, response) => {
     return response.json([
         'Projeto 5',
         'Projeto 2',
