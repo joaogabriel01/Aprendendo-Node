1367f0a9ea80b689b226b2856fa7c5f4f52bce27
joaoazevedo <joaogfazevedo@gmail.com>
2022-01-13 00:27:00 -0300

    Ate a aula 6

---
+++ src/index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+
+const app = express();
+
+
+app.use(express.json())
+// app.get('/projects', (request, response) => {
+//     return response.send('{message: Hello World!}');
+// });
+app.get('/projects', (request, response) => {
+    const query = request.query;
+    console.log(query);
+    return response.json([
+        'Projeto 1',
+        'Projeto 2',
+        'Projeto 3'
+    ]);
+});
+
+app.post('/projects', (request, response) => {
+    const body = request.body;
+    console.log(body);
+    return response.json([
+        'Projeto 1',
+        'Projeto 2',
+        'Projeto 3',
+        'Projeto 4'
+    ]);
+})
+
+app.put('/projects/:id/owner/:owner', (request, response) => {
+
+    const id = request.params; 
+    console.log(id);
+
+    return response.json([
+        'Projeto 5',
+        'Projeto 2',
+        'Projeto 3',
+        'Projeto 4'
+    ]);
+})
+
+app.delete('/projects', (request, response) => {
+    return response.json([
+        'Projeto 5',
+        'Projeto 2',
+        'Projeto 3'
+    ]);
+})
+
+app.listen(3333, () => {
+    console.log("Backend started!")
+});
